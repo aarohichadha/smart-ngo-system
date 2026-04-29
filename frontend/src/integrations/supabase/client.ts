192cf6e8b39594152ef6c027ffdec9cb925e440f
Aarohi Chadha <149961233+aarohichadha@users.noreply.github.com>
2026-04-29 13:54:20 +0530

    Log Supabase URL and Publishable Key
    Added console logs for SUPABASE_URL and SUPABASE_PUBLISHABLE_KEY. ( A temproray change for deployment)

--- frontend/src/integrations/supabase/client.ts
+++ frontend/src/integrations/supabase/client.ts
@@ -5,8 +5,8 @@ import type { Database } from './types';
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
-// Import the supabase client like this:
-// import { supabase } from "@/integrations/supabase/client";
+console.log("SUPABASE_URL:", SUPABASE_URL);
+console.log("SUPABASE_KEY:", SUPABASE_PUBLISHABLE_KEY);
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
   auth: {
